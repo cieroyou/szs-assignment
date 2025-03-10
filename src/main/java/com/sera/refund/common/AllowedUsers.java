38f3d67264aa7f5777530238c66fb998ade207e7
sera <cieroyou1@gmail.com>
2025-03-10 17:51:57 +0900

    회원가입 시 허용된 사용자만 회원가입 가능하도록 validation 추가

---
+++ src/main/java/com/sera/refund/common/AllowedUsers.java
@@ -0,0 +1,27 @@
+package com.sera.refund.common;
+
+
+import java.util.Map;
+import java.util.Set;
+
+public class AllowedUsers {
+
+    // 🔹 사전 등록된 사용자 (이름 -> 주민등록번호)
+    private static final Map<String, String> ALLOWED_USERS = Map.of(
+            "동탁", "921108-1582816",
+            "관우", "681108-1582816",
+            "손권", "890601-2455116",
+            "유비", "790411-1656116",
+            "조조", "810326-2715702"
+    );
+
+    // 🔹 회원가입 가능한 이름 목록 가져오기
+    public static Set<String> getAllowedNames() {
+        return ALLOWED_USERS.keySet();
+    }
+
+    // 🔹 이름과 주민등록번호가 일치하는지 확인
+    public static boolean isAllowedUser(String name, String regNo) {
+        return ALLOWED_USERS.containsKey(name) && ALLOWED_USERS.get(name).equals(regNo);
+    }
+}
--- src/main/java/com/sera/refund/service/SignupService.java
+++ src/main/java/com/sera/refund/service/SignupService.java
@@ -1,5 +1,6 @@
 package com.sera.refund.service;
 
+import com.sera.refund.common.AllowedUsers;
 import com.sera.refund.controller.UserSignupRequest;
 import com.sera.refund.domain.User;
 import com.sera.refund.domain.UserRepository;
@@ -16,6 +17,13 @@ public class SignupService {
 
     public void registerUser(UserSignupRequest request) {
         // todo: 이미 존재하는지 체크
+
+        // 허용된 사용자만 가입하도록 함
+        if (!AllowedUsers.isAllowedUser(request.getName(), request.getRegNo())) {
+            // TODO: 예외를 Class 로 정의하여 유지보수성 높일 것
+            throw new IllegalArgumentException("허용되지 않은 사용자입니다.");
+        }
+
         String encodedPassword = passwordEncoder.encode(request.getPassword());
         String encodedRegistrationNo = passwordEncoder.encode(request.getRegNo());
         User user = User.of(request.getUserId(), encodedPassword, request.getName(), encodedRegistrationNo);
