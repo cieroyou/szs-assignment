5dfd042c3b15376012b32b4e9bd168ce2da77790
sera <cieroyou1@gmail.com>
2025-03-11 22:15:24 +0900

    변경된 로그인 방식으로 테스트코드 동작하도록 수정

--- src/test/java/com/sera/refund/service/LoginServiceTest.java
+++ src/test/java/com/sera/refund/service/LoginServiceTest.java
@@ -3,7 +3,6 @@ package com.sera.refund.service;
 import com.sera.refund.common.TokenProvider;
 import com.sera.refund.controller.dto.UserLoginRequest;
 import com.sera.refund.domain.User;
-import com.sera.refund.infrastructure.UserRepository;
 import com.sera.refund.exception.BaseException;
 import com.sera.refund.exception.ErrorCode;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,11 +29,10 @@ class LoginServiceTest {
     @InjectMocks
     private LoginService loginService;
     @Mock
-    private BCryptPasswordEncoder passwordEncoder;
+    TokenProvider tokenProvider;
     @Mock
-    private UserRepository userRepository;
+    AuthenticationManager authenticationManager;
     @Mock
-    private TokenProvider tokenProvider;
     private User 관우;
 
     @BeforeEach
@@ -41,36 +44,46 @@ class LoginServiceTest {
     @Test
     void 로그인_성공() {
         // given
+        String token = "mocked-token";
         UserLoginRequest request = new UserLoginRequest("kw1", "password");
-        when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.of(관우));
-        when(passwordEncoder.matches(request.getPassword(), 관우.getPassword())).thenReturn(true);
-        when(tokenProvider.generateToken(관우.getUserId())).thenReturn("token");
+        Authentication authToken = new UsernamePasswordAuthenticationToken(request.getUserId(), request.getPassword());
+        Authentication authResult = new UsernamePasswordAuthenticationToken(request.getUserId(), null, Collections.emptyList());
+
+        when(authenticationManager.authenticate(authToken)).thenReturn(authResult);
+        when(tokenProvider.generateToken(request.getUserId())).thenReturn(token);
 
         // when
-        String token = loginService.login(request);
+        String resultToken = loginService.login(request);
 
         // then
-        assertEquals("token", token);
+        assertEquals("mocked-token", resultToken);
     }
 
     @Test
     void 로그인_실패_아이디_없음() {
         // given
         UserLoginRequest request = new UserLoginRequest("kw1", "password");
-        when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.empty());
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(request.getUserId(), request.getPassword());
+
+        when(authenticationManager.authenticate(token))
+                .thenThrow(new BaseException(ErrorCode.USER_NOT_FOUND, "User not found"));
 
         // when
         // then
         BaseException exception = assertThrows(BaseException.class, () -> loginService.login(request));
         assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
     }
-
+//
     @Test
     void 로그인_실패_비밀번호_불일치() {
         // given
         UserLoginRequest request = new UserLoginRequest("kw1", "password");
-        when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.of(관우));
-        when(passwordEncoder.matches(request.getPassword(), 관우.getPassword())).thenReturn(false);
+        Authentication authToken = new UsernamePasswordAuthenticationToken(request.getUserId(), request.getPassword());
+
+        when(authenticationManager.authenticate(authToken))
+                .thenThrow(new BaseException(ErrorCode.INVALID_PASSWORD, "Invalid user ID or password"));
+
 
         // when
         // then
